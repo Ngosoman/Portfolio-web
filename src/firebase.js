9e0a4e4042dac3d55405254a154b6239125f0b0a
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-06 14:20:10 +0300

    refactor: Comment out Firebase initialization code in firebase.js and firebase-comment.js

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore"
+// import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui ini
+
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+// apiKey: "AIzaSyCh2LAoF8RNGVZvdLL2DSuDXCCem-iNwVo",
+//   authDomain: "react-eba8b.firebaseapp.com",
+//   projectId: "react-eba8b",
+//   storageBucket: "react-eba8b.firebasestorage.app",
+//   messagingSenderId: "513535066519",
+//   appId: "1:513535066519:web:21a71982c0c68cb6b8c4d2",
+//   measurementId: "G-JHKEC60KE2"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+
+// export { db, collection, addDoc };
