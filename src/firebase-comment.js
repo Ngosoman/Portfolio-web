d43773d302a8b3ecc81aa4af0bb2016afa54bd2d
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-07 10:53:19 +0300

    Updated Firebase comment and firebase.js

--- src/firebase-comment.js
+++ src/firebase-comment.js
@@ -1,20 +1,19 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage";
-// import { collection, addDoc } from "@firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDJPq9a0YPoQYkpQ-Uaw7aXQRXzzqOKzFA",
-//     authDomain: "web-kelas-tes.firebaseapp.com",
-//     projectId: "web-kelas-tes",
-//     storageBucket: "web-kelas-tes.appspot.com",
-//     messagingSenderId: "890817433268",
-//     appId: "1:890817433268:web:11e5258f8864a6174c11e1"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDJPq9a0YPoQYkpQ-Uaw7aXQRXzzqOKzFA",
+  authDomain: "web-kelas-tes.firebaseapp.com",
+  projectId: "web-kelas-tes",
+  storageBucket: "web-kelas-tes.appspot.com",
+  messagingSenderId: "890817433268",
+  appId: "1:890817433268:web:11e5258f8864a6174c11e1"
+};
 
-// // Initialize with a unique name
-// const app = initializeApp(firebaseConfig, 'comments-app');
-// const db = getFirestore(app);
-// const storage = getStorage(app);
+// You don't need a custom name unless using multiple apps
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-// export { db, storage, collection, addDoc };
+export { db, storage, collection, addDoc };
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,17 @@
-// import { initializeApp } from "firebase/app";
-// import { getFirestore } from "firebase/firestore"
-// import { collection, addDoc, getDocs } from "@firebase/firestore"; // Perbarui ini
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyCh2LAoF8RNGVZvdLL2DSuDXCCem-iNwVo",
+  authDomain: "react-eba8b.firebaseapp.com",
+  projectId: "react-eba8b",
+  storageBucket: "react-eba8b.appspot.com", // 🔥 fixed `.app` to `.appspot.com`
+  messagingSenderId: "513535066519",
+  appId: "1:513535066519:web:21a71982c0c68cb6b8c4d2",
+  measurementId: "G-JHKEC60KE2"
+};
 
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-// apiKey: "AIzaSyCh2LAoF8RNGVZvdLL2DSuDXCCem-iNwVo",
-//   authDomain: "react-eba8b.firebaseapp.com",
-//   projectId: "react-eba8b",
-//   storageBucket: "react-eba8b.firebasestorage.app",
-//   messagingSenderId: "513535066519",
-//   appId: "1:513535066519:web:21a71982c0c68cb6b8c4d2",
-//   measurementId: "G-JHKEC60KE2"
-// };
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const db = getFirestore(app);
-
-// export { db, collection, addDoc };
+export { db, collection, addDoc, getDocs };
