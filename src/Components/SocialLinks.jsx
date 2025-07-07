231807e6e37127f57a10f048982b14f4fdf55ef7
Tom Mwambingu <[tomchom30@gmail.com]>
2025-07-07 15:22:47 +0300

    Updated tiktok

--- src/Components/SocialLinks.jsx
+++ src/Components/SocialLinks.jsx
@@ -62,7 +62,7 @@ const socialLinks = [
     </g>
 </svg>
     ),
-    url: "https://tiktok.com/@user12768883398676",
+    url: "https://tiktok.com/@user12768883398667",
     color: "black",
     gradient: "from-[#000000] via-[#25F4EE] to-[#FE2C55]"
   }
